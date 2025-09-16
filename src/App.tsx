90d02883c1e365f0b77a4dc5c644baf0bc4a6715
Anahit Gulian <anahit@civiqs.com>
2025-09-16 12:41:15 -0400

    add wouter and Home and About routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
+import { Route, Switch } from "wouter";
+import { Home } from "./Home";
+import { About } from "./About";
+
+const App = () => {
   return (
-    <>
-
-    </>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route>404: No such page!</Route>
+    </Switch>
   )
-}
+};
 
-export default App
+export { App };
